d45603cd15b2c7a81bd097aaf17f5acae9164e29
Dan Richert <dan.richert@gmail.com>
2017-08-26 21:55:06 -0400

    get sensor data by timestamp

--- server/sensorData.js
+++ server/sensorData.js
@@ -4,29 +4,50 @@ var AWS = require("aws-sdk")
 var config = require("./config.json")
 var db = new AWS.DynamoDB(config)
 
-// options -
-//   sensor: Sensor number (1-4)
-var sensorData = (options) => {
-  return new Promise((resolve, reject) => {
+// time - Creation arg for moment() (default: now)
+//   See https://momentjs.com/docs/
+var sensorData = (time = moment()) => {
+  // Default to now, multiple by 1000 to get ms timestamp
+  // format used in database
+  //if(!time) time = +new Date * 1000
+  time = moment(time)
+
+  var sensors = [1, 2, 3, 4]
+
+  var promises = sensors.map(sensor => {
     let params = {
-      ExpressionAttributeValues: {
-        ":v1": { N: "" + options.sensor }
+      ExpressionAttributeNames: {
+        "#T": "timestamp"
       },
-      KeyConditionExpression: "sensor = :v1",
-      TableName: "ff-tgs2602-voc"
+      ExpressionAttributeValues: {
+        ":sensor": { N: "" + sensor },
+        ":ms": { N: "" + time.unix() }
+      },
+      KeyConditionExpression: "sensor = :sensor AND #T <= :ms",
+      TableName: "ff-tgs2602-voc",
+      ScanIndexForward: false,
+      Limit: 100
     }
 
-    db.query(params, (err, data) => {
-      if(err) reject(err)
-      else {
-        let values = data.Items.map(item => {
-          return item.value.N
-        })
+    return new Promise((resolve, reject) => {
+      db.query(params, (err, data) => {
+        if(err) reject(err)
+        else {
+          let values = data.Items.map(item => {
+            return {
+              sensor: item.sensor.N,
+              timestamp: item.timestamp.N,
+              value: item.value.N
+            }
+          })
 
-        resolve(values)
-      }
+          resolve(values)
+        }
+      })
     })
   })
+
+  return Promise.all(promises)
 }
 
 module.exports = sensorData
