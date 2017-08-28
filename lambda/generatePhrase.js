067427ec0ef2e238dccf996f5909ef2347686891
Dan Richert <dan.richert@gmail.com>
2017-08-28 14:26:20 -0400

    extract sensor values in lambda

--- lambda/generatePhrase.js
+++ lambda/generatePhrase.js
@@ -8,7 +8,7 @@ var fn = (event, context, cbk) => {
   inputs = [99, 22, 77, 300]
 
   phrase = gen.generate(inputs)
-  console.log(phrase)
+  //console.log(phrase)
 
   cbk(null, phrase)
 }
@@ -19,35 +19,46 @@ var fn = (event, context, cbk) => {
 
 //exports.handler = (event, context, cbk) => {
 module.exports = (event, context, cbk) => {
-  var getLatestPhraseTimestamp = () => {
-    let params = {
-      AttributesToGet: [ "timestamp" ],
-      TableName: "phrase-test",
-      Key: ""
-    }
+  //var getLatestPhraseTimestamp = () => {
+  //  let params = {
+  //    AttributesToGet: [ "timestamp" ],
+  //    TableName: "phrase-test",
+  //    Key: ""
+  //  }
 
-    db.query(params)
-  }
+  //  db.query(params)
+  //}
 
-	var putPhrase = (phrase, timestamp) => {
-    let params = {
-      Item: {
-        type: { S: "phrase" },
-        timestamp: { N: timstamp },
-        phrase: { S: phrase }
-      }
-			TableName: "phrase-test"
-		}
+  //var putPhrase = (phrase, timestamp) => {
+  //  let params = {
+  //    Item: {
+  //      type: { S: "phrase" },
+  //      timestamp: { N: timstamp },
+  //      phrase: { S: phrase }
+  //    }
+  //    TableName: "phrase-test"
+  //  }
 
-    return new Promise((resolve, reject) => {
-      db.putItem(params, (err, data) => {
-        if(err) reject(err)
-        else resolve(data)
-      })
-    })
-  }
+  //  return new Promise((resolve, reject) => {
+  //    db.putItem(params, (err, data) => {
+  //      if(err) reject(err)
+  //      else resolve(data)
+  //    })
+  //  })
+  //}
 
-  var getVal = (sensor, timestamp = +new Date) => {
+  var records = event.Records.map(r => {
+    return r
+  })
+
+  //var timestamps = event.Records.map(r => {
+  //  return r.dynamodb.Keys.timestamp.N
+  //})
+
+  //console.log("TIMESTAMPS", timestamps)
+  var timestamp = event.Records[0].dynamodb.Keys.timestamp.N
+
+  var getVal = (sensor) => {
     let params = {
       ExpressionAttributeNames: {
         "#T": "timestamp"
@@ -56,7 +67,7 @@ module.exports = (event, context, cbk) => {
         ":sensor": { N: "" + sensor },
         ":ms": { N: "" + timestamp }
       },
-      KeyConditionExpression: "sensor = :sensor AND #T < :ms",
+      KeyConditionExpression: "sensor = :sensor AND #T = :ms",
       TableName: "sensor-test-3",
       ScanIndexForward: false,
       Limit: 1
@@ -66,8 +77,8 @@ module.exports = (event, context, cbk) => {
       db.query(params, (err, data) => {
         if(err) reject(err)
         else {
-          console.log("GOT DATA", JSON.stringify(data))
-          resolve(data)
+          //console.log("GOT DATA", JSON.stringify(data))
+          resolve(data.Items[0].value.N)
         }
       })
     })
